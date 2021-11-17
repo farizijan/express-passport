da29ac2ae19263adb10fc400863148972bcb65a2
Fariz Izzan <fiagusta@gmail.com>
2021-11-17 14:15:57 +0700

    implement passport-local strategy

--- app.js
+++ app.js
@@ -1,18 +1,40 @@
 var express = require('express');
+var passport = require('passport');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+var LocalStrategy = require('passport-local').Strategy;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+passport.use(new LocalStrategy(
+    function(uname, pass, done) {
+        var valid = uname == 'fariz' && pass == 'fariz';
+        var user = { name : "Fariz Izzan", role: "Backend Developer" };
+        if (!valid) { return done(null, false); }
+        return done(null, user);
+    }
+));
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
+// app.use(passport.authenticate('session'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 
 /* GET home page. */
@@ -6,4 +7,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/' }),
+  function(req, res) {
+    res.send('Login success!');
+  }
+);
 module.exports = router;
